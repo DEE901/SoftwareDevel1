fbce206ae074927657226c314aa1ca8e8dbf2e7d
DEE901 <Deegan.hansen@malad.us>
2025-09-15 17:06:30 -0600

    Create array.java
    2.1 array

---
+++ array.java
@@ -0,0 +1,17 @@
+//This is my 2.1 assinment
+// by Deegan
+//this is a array
+
+public class array {
+
+
+    public static void main(String[] args){
+        //   int[] diceRolls = new int[6];
+        int []diceRolls={3,5,1,2,6};
+        for (int i=0;i< diceRolls.length;i++){
+            System.out.println(diceRolls[i]);
+        }
+    }
+} {
+    
+}
