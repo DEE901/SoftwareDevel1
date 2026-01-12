da8aaa1401d1073d3d16d7fa4ffa640c6027404b
DEE901 <Deegan.hansen@malad.us>
2026-01-12 15:41:41 -0700

    Add files via upload
    I hope this works

---
+++ import java.util.Scanner.java
@@ -0,0 +1,18 @@
+// by Deegan
+// for software class project
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        greetUser(name);
+        scanner.close();
+    }
+
+    public static void greetUser(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
