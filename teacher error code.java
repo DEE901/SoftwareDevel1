f7ce5591a8018c79fb40431ec2c90847e0993114
DEE901 <Deegan.hansen@malad.us>
2026-03-24 15:53:46 -0600

    Add files via upload
    First code hello world code: it was missing a semicolon and had a colon where a semicolon should have been.
    Second code hello world bad code: you used a = when you should have used a == symbol, also i = 1 returns an int, not a Boolean, so it would not compile.
    Third code error: There were a lot of errors in this one. The first one, Java, does not allow executable statements directly inside a class. These must be inside a method like main, then your code has no entry point. You also mistyped printlm, it should be println. Your for should be lowercased, and so should your int. You put a : where a ; should be, and you also put i > 10, which means it will not run. You should have also capitalized the word system.

---
+++ teacher error code.java
@@ -0,0 +1,25 @@
+// your fixed error code
+//Deegan
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("hello everybody");
+        System.out.println("am I doing this correctly");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("the number i is " + i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("the new number i is " + i);
+        }
+
+        int i = 0;
+        if (i == 0) {
+            System.out.print("i is equal to zero");
+        } else {
+            System.out.print("i is not equal to zero");
+        }
+    }
+}
---
+++ teacher hello world bad code.java
@@ -0,0 +1,14 @@
+// your fixed hello world bad code
+//Deegan
+
+class HelloWorldBad { 
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 25; i++) {
+            System.out.println("the value of i is " + i);
+            if (i == 1) {
+                System.out.println("i is equal to 1");
+            }
+        }    
+    }
+}
